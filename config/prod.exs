d0a3a8aeafdcdf18a6b60aa055f74336715fd868
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-04-04 16:56:14 -0400

    add buildpacks and release

--- config/prod.exs
+++ config/prod.exs
@@ -3,7 +3,6 @@ use Mix.Config
 config :van_matcher, VanMatcher.Endpoint,
   http: [:inet6, port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json",
   server: true
 
 # Do not print debug messages in production
